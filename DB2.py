f139edceb8a62f65d41a8c60dab4f4f305f3e948
rdrophin <rophin@naver.com>
2024-04-17 10:01:36 +0900

    데이터베이스 연습 완료

--- DB2.py
+++ DB2.py
@@ -1,13 +1,19 @@
-#DB1.py
+#DB2.py
 import sqlite3
 
 #일단 메모리에서 임시 저장
-con = sqlite3.connect(":memory:")
+#con = sqlite3.connect(":memory:")
+#파일로 저장
+#con = sqlite3.connect("c:\\work\\test.db")
+#파일로 저장(커밋까지 완료)
+con = sqlite3.connect("c:\\work\\sample.db")
+
 #커서 객체
 cur = con.cursor()
 
 #테이블(자료구조)생성
-cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+cur.execute(
+    "create table if not exists PhoneBook (Name text, PhoneNum text);")
 
 #1건 입력
 cur.execute("insert into PhoneBook values ('홍길동','010-222')")
@@ -21,18 +27,9 @@ cur.execute("insert into PhoneBook values (?,?);",(name, phoneNum))
 datalist = (("고길동", "010-123"),("박문수","010-567"))
 cur.executemany("insert into PhoneBook values (?,?);",datalist)
 
-
-#검색
-cur.execute("select * from PhoneBook;")
-#for row in cur:
-#    print(row[0],row[1])
-#for row in cur:
-#    print(row)
-
-print("---fetchone()---")
-print(cur.fetchone())
-print("---fetchmany(2)---")
-print(cur.fetchmany())
 print("---fetchall()---")
 cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
+
+#작업 정상 완료
+con.commit()
